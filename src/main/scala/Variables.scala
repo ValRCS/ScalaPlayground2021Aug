52aa5feb74fe994e48a104be791c734f08899126
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-08-10 21:58:36 +0300

    Double precision data types and round

--- src/main/scala/Variables.scala
+++ src/main/scala/Variables.scala
@@ -30,4 +30,34 @@ object Variables extends App {
 //  myNumber = "Cant do this" //in Scala you can not change data types of variables , of course values can not be changed at all
   val myString:String = "Just a string" //again :String is not required
   println(myString)
+
+  //one of the biggest challenges in programming is how to name things :)
+  // x and y are great val and var names if they refer to x and y coordinates
+  // x and y are not so good for say saving your name or your age
+
+  val myName = "Valdis" //so this would be a good val name
+  val lastName = "Saulespurens"
+  println(s"Why hello there $myName $lastName")
+
+  //in Scala we use something called camelCase for values, variables and many other things
+  //but not for Objects those start with CapitalLetter
+  val l = 333 //bad name, since l in many fonts is hard to distinguish from 1 or I
+
+  //so takeaway is to use reasonably short but descriptive names for your variables
+
+  val priceSalt = 0.27 //this is new data type - double, meaning it is a double precision floating point number
+  val kgBought = 15
+  val purchaseTotal = priceSalt * kgBought
+
+  val purchaseMessage = s"$myName bought $kgBought kilos of salt at $priceSalt each. He paid $purchaseTotal total"
+  println(purchaseMessage)
+
+  val purchaseRounded = purchaseTotal.round //so round actually rounds to nearest integer
+  println(s"Actually the total price was $purchaseRounded Euros")
+
+  //TODO round to 2 decimal places
+  //so with TODO we add a note to ourselves and colleagues to do something undone
+
+  //FIXME is more serious but similar note, means something is not quite right
+
 }
