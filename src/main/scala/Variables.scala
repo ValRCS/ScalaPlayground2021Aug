ebbf1e94ce6a4211a076c628c2597673f85aa58c
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-08-10 20:08:39 +0300

    Initial commit

---
+++ src/main/scala/Variables.scala
@@ -0,0 +1,16 @@
+object Variables extends App {
+  println("Scala has two kind of variables, val and var")
+  val a = 15 //values stay constant, in some other languages they are called constants
+  val b = 6
+  var c = a + b //so c can be changed as many times as we wish (as long as it remains an whole number  integer)
+  println(a,b,c)
+  c = a*b //values can not be changed
+  //in Scala we start by using values and only when absolutely necessary we use variables
+  println(a,b,c)
+  c = 55
+  println(a,b,c)
+//  println(a,b,c,d) //bad idea we are using d before it is created
+  val d = a+b+c
+  println(a,b,c,d)
+
+}
