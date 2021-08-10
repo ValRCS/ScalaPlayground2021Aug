7a16a01f3404dbcfb5ff7b7c3e0349dbc107422e
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-08-10 21:33:15 +0300

    Declaring data types in Scala

--- src/main/scala/Variables.scala
+++ src/main/scala/Variables.scala
@@ -14,4 +14,20 @@ object Variables extends App {
   println(a,b,c,d)
   println("a+b=", a+b)
 
+  val msg = "Hello, world!"
+  println(msg)
+  val mySum = a+b
+  println(s"$a added to $b equals $mySum") //using s - string interpolation it lets us add variables inside our strings
+
+  //Scala is strongly statically typed - meaning each variable and value has a specific type
+
+  val myInteger:Int = 20 // :Int is not required , because Scala can figure out the type from 20
+
+  var myNumber = 15
+  println(myNumber)
+  myNumber = 20
+  println(myNumber)
+//  myNumber = "Cant do this" //in Scala you can not change data types of variables , of course values can not be changed at all
+  val myString:String = "Just a string" //again :String is not required
+  println(myString)
 }
