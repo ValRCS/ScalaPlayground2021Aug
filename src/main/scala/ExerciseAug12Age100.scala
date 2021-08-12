c6ae131e41fa385966e264e6063dbb889909ee2f
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-08-12 20:16:03 +0300

    Exercise for Reading user input

---
+++ src/main/scala/ExerciseAug12Age100.scala
@@ -0,0 +1,9 @@
+object ExerciseAug12Age100 extends App {
+  //TODO Ask user for the current Year
+  //TODO ask user for their name
+  //TODO ask user for their age...
+
+  //TODO calculate and print the year when the user will be 100 years old
+
+  //remember about typecasting to numeric types from string
+}
