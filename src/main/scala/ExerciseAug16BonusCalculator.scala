d7dfdf0c8b116adbafbb2e886ad905a1dd92abed
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-08-16 20:15:39 +0300

    Exercise for Aug 16 on Bonus

---
+++ src/main/scala/ExerciseAug16BonusCalculator.scala
@@ -0,0 +1,13 @@
+object ExerciseAug16BonusCalculator extends App {
+  println("Xmas bonus calculator")
+  //TODO
+  //Ask for person's name
+  //Ask for how long they have worked at the firm
+  //Ask for monthly wage
+  //Calculate Xmas bonus if they have worked at least 2  years
+  //Bonus is years worked over 2 years * 15% of monthly wage
+  //so 5 years worked, 1000 Euros wage would give 450 Euro bonus (3 years * 150)
+
+  //if they are not eligible for bonus you should say so
+
+}
