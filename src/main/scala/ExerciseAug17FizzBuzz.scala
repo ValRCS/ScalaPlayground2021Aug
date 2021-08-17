cef5d2ed8202f3e1eb833748583c53da93f81f09
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-08-17 20:10:18 +0300

    FizzBuzz exercise

---
+++ src/main/scala/ExerciseAug17FizzBuzz.scala
@@ -0,0 +1,9 @@
+object ExerciseAug17FizzBuzz extends App {
+  //TODO create a FizzBuzz program based on children's game I think from Britain
+  //for numbers 1 to 100 (included!)
+  //print FizzBuzz if number divides evenly by both 5 and 7
+  //print Fizz if number only divides by 5
+  //print Buzz if number divides by 7
+  //print number if number does not divide by 5 or 7
+  //so something like this 1,2,3,4,Fizz,6,Buzz,..... 34,FizzBuzz,36,....
+}
