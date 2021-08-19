0556001aeea126c1fbdf8c363fa19c4f57ca3400
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-08-19 20:08:27 +0300

    Exercise for Aug 19 Farenheit from Celsius

---
+++ src/main/scala/ExerciseAug19CityPopulation.scala
@@ -0,0 +1,22 @@
+object ExerciseAug19CityPopulation extends App {
+  println("Function to calculate city growth")
+  //TODO write a function cityPop which takes the following 4 parameters
+  //p0: Int - how many people are in beginning
+  //perc: Int - yearly growth rate in percentages
+  //delta: Int - how many people immigrate(+)/emigrate to the city each year
+  //targetPopulation: Int - population we want to reach
+
+  //we want to return the year city will reach targetPopulation
+  //or we return -1 if the city will NEVER reach the population
+
+  def cityPopulation(p0: Int, percentage: Int, delta: Int, targetPopulation: Int):Int = {
+    9000 //FIXME
+  }
+
+  //write a function (name it yourself) to calculate Farenheit from Celsius
+  //f = 32 + c*9/5
+
+  //test it with 36.6
+  //test it with 37
+  //test it with 48.5
+}
