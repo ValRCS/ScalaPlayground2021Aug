ae9e10ea8c194db4f1309cb39731c38fdd4761a0
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-08-23 21:09:33 +0300

    String looping exercise half solved

--- src/main/scala/ExerciseAug23StringAdjustment.scala
+++ src/main/scala/ExerciseAug23StringAdjustment.scala
@@ -4,6 +4,10 @@ object ExerciseAug23StringAdjustment extends App {
     //one approach could be to have a variable which we keep changing each time for replacement with uppercase
     var resultText = text
     for (char <- uppercaseChars) {
+      //TODO we want to replace each character by character inside the loop
+      println(s"Working on making $char uppercase to ${char.toUpper}")
+      resultText = resultText.replace("","") //FIXME
+      //we can not replace text inside string but we can create a new string out of old string
       //you want to replace each of the characters with their uppercase version
       //so the idea is to use replace on result Text...
     }
