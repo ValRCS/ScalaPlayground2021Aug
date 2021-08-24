cfa54cbbe0b1a0ee828aa74bcfdf13ef487d39f3
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-08-24 21:56:19 +0300

    Array Concatenation (combining)

--- src/main/scala/ArrayConcatenation.scala
+++ src/main/scala/ArrayConcatenation.scala
@@ -7,4 +7,8 @@ object ArrayConcatenation extends App  {
   //we can get creative and go from Ranges immediately to a completed IndexedSeq
   val numbersFromRanges = (1 to 5) ++ (30 to 33) ++ (55 to 59) ++ (-7 to -3) ++ (1000 to 960 by -4)
   println(numbersFromRanges.mkString(","))
+  println(numbersFromRanges.sum, numbersFromRanges.min, numbersFromRanges.max)
+
+  val sortedNumbers = numbersFromRanges.sorted //so ascending by default
+  println(sortedNumbers.mkString(","))
 }
