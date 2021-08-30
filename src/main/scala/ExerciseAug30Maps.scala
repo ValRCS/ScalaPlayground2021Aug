2bace4ad5c8745505291588826f3129232c5535c
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-08-30 21:37:45 +0300

    Map exercise Solved

--- src/main/scala/ExerciseAug30Maps.scala
+++ src/main/scala/ExerciseAug30Maps.scala
@@ -10,9 +10,27 @@ object ExerciseAug30Maps extends App {
     //TODO
     //val myMap which contains keys of each word and values would be integers of word length
     //remember each string has attribute length.
-    for (word <- words) {
-      println(word, word.length)
-    }
+
     //you can use yield to create immutable map , or use the above for loop and mutable Map
     //println(myMap)
+    val myMap = (for (word <- words) yield (word, word.length)).toMap
+    println(myMap)
+
+  //less recommended would be to use a mutable map //this is how many older languages do it
+  val mutableMap = scala.collection.mutable.Map[String, Int]() //an empty Map to be filled
+  for (word <- words) {
+    println("Adding", word, word.length)
+    mutableMap += (word -> word.length)
+  }
+  println(mutableMap)
+
+  //contents of both are same but order is different
+  //if you really want the insertion order(not usually important)
+  //we can use mutable  LinkedHashMap
+  val mutableLinkedHashMap = scala.collection.mutable.LinkedHashMap[String, Int]() //an empty Map to be filled
+  for (word <- words) {
+    println("Adding", word, word.length)
+    mutableLinkedHashMap += (word -> word.length)
+  }
+  println(mutableLinkedHashMap)
 }
