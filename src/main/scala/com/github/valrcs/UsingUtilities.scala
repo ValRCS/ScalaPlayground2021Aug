6fed0da0c92ac83b480889d7218189bfa2619671
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-09-06 21:49:09 +0300

    using classes from root of the project

--- src/main/scala/com/github/valrcs/UsingUtilities.scala
+++ src/main/scala/com/github/valrcs/UsingUtilities.scala
@@ -1,5 +1,10 @@
 package com.github.valrcs //notice this was created automatically by intellij because we started in our package location
+import _root_.Car //so if i have something at the root of the project that I need to import I can use this _root_
 
 object UsingUtilities extends App {
   println(Utilities.MYP) //so I can other objects, classes etc from the same package
+
+  val newCar = Car("Bugatti") //i had to import Car from _root_.Car because it is in the root of the project without package
+
+  println(newCar)
 }
