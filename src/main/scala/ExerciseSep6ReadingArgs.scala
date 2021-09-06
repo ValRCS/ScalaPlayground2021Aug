13a8a1911ae48563c1f9884bb46b69684eb42c55
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-09-06 20:01:44 +0300

    Reading Arguments and Exercise Sep 6

---
+++ src/main/scala/ExerciseSep6ReadingArgs.scala
@@ -0,0 +1,12 @@
+object ExerciseSep6ReadingArgs {
+  //TODO either with extend App or def main (choose one!)
+
+  //TODO read all arguments
+  //TODO print how many arguments were entered
+  //TODO cast all arguments to double
+  //TODO print sum of all arguments
+
+  //TODO print average of all arguments
+
+  //TODO use Run -> Edit Configuration  to add some arguments separated by space to your application
+}
