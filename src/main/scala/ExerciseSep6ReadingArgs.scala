7379480c708eb98c1dc42b8194e5f2dfb7f8fc46
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-09-06 21:17:53 +0300

    Three ways of solving Sep 6 exercise

--- src/main/scala/ExerciseSep6ReadingArgs.scala
+++ src/main/scala/ExerciseSep6ReadingArgs.scala
@@ -1,7 +1,34 @@
 object ExerciseSep6ReadingArgs {
   //TODO either with extend App or def main (choose one!)
+  def main(args:Array[String]) = {
+    println("Starting main function")
+    //TODO read all arguments
+    val argCount = args.length
+    println(s"We got ${argCount} arguments")
+//    var total = 0.0
+    var total:Double = 0 //also works
+    for (arg <- args) {
+      println(s"Going to do something with argument: $arg its type is ${arg.getClass}")
+      total += arg.toDouble
+    }
+    println(s"Sum of all elements is $total")
 
-  //TODO read all arguments
+    //all of the arguments in args are strings
+    //so assuming we know those are strings that can be numbers
+    //we could do the following which is more functional programming like
+    val myNumbers = args.map(_.toDouble) //same as (item => item.toDouble)
+    val numberSum = myNumbers.sum //so same as total
+    println(s"Also sum of all elements is $numberSum")
+
+    //we could also do the yield which as we remember translate to map underneath
+    val myNumbersAlso = for (arg <- args) yield arg.toDouble
+    val numberSumAlso = myNumbersAlso.sum
+    println(s"Finally sum of all elements is $numberSumAlso")
+
+    val avg = total / argCount
+    println(s"Average of all elements is $avg")
+  }
+
   //TODO print how many arguments were entered
   //TODO cast all arguments to double
   //TODO print sum of all arguments
