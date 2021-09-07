9373863f0ab38cfc95d0b07df09914993ad21962
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-09-07 20:01:01 +0300

    Big Pattern Matching example

--- src/main/scala/com/github/valrcs/Utilities.scala
+++ src/main/scala/com/github/valrcs/Utilities.scala
@@ -7,4 +7,29 @@ object Utilities {
   val MYP = 3.1415926
   def add(a:Int, b:Int): Int = a+b //functions that are to be used by others should have full type information
   //if we forget Intellij will remind us at some point
+
+  /**
+   *
+   * @param monthIndex
+   * @return month of the year as string
+   *         note: valid months are 1 to 12
+   */
+  def getMonth(monthIndex: Int):String = {
+    monthIndex match {
+      case 1 => "January"
+      case 2 => "February"
+      case 3 => "March"
+      case 4 => "April"
+      case 5 => "May"
+      case 6 => "June"
+      case 7 => "July"
+      case 8 => "August"
+      case 9 => "September"
+      case 10 => "October"
+      case 11 => "November"
+      case 12 => "December"
+      //this leaves space to add extra month, really easy to add extra capabilities
+      case _ => "Invalid month" // the default, catch-all
+    } //as this is pattern matcher is the last item in our function the result gets returned
+  }
 }
