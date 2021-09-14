e017de06bc2da99fadca5c19e582fae4f2f07b21
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-09-14 21:56:08 +0300

    Getting Start Points of all stories but one

--- src/main/scala/com/github/valrcs/ExerciseSep14ExtractStories.scala
+++ src/main/scala/com/github/valrcs/ExerciseSep14ExtractStories.scala
@@ -45,9 +45,32 @@ object ExerciseSep14ExtractStories extends App {
   //so we want all text that is not first word
   val storyNames = for (line <- tocLines if line.nonEmpty) yield {
     //TODO do some more work here in preparing the line
-
-    line
+    //idea trim each line then get everything but first word(which is Roman numeral in this case)
+    val words = line.trim.split(" +")
+    //so tail will give me array or other sequence with everything BUT first item, we could have use slice also but then we'd need length
+    val title = words.tail.mkString(" ") // only catch is that we lost any extra whitespace between words
+    title
   } //this should filter empty lines
   println("*"*40) //just some divider for visuals
   storyNames.foreach(println) //so we do have all the stories we just want to get rid of Roman numerals
+
+  //now that we have our story names, we can go look for the stories
+  //this will be a bit more difficult because we need to figure out where the story ends
+
+//  val firstStoryTitle = storyNames.head
+
+  def findLineNumber(lines:Array[String], toMatch:String):Int = {
+    //so this not Scala style using early return but very convenient here
+    for ((line, index) <- lines.zipWithIndex) {
+      if (line.trim == toMatch) {
+        println(s"Found Story: $toMatch at line ${index+1}") //zipIndex starts with 0 but text file starts at 1
+        return index+1 //Scala does not like early return but here it is too convenient to avoid
+      }
+    }
+    0 //nothing found
+  }
+
+  val storyStarts = storyNames.map(story => findLineNumber(lines, story))
+  println(storyStarts.mkString(","))
+
 }
