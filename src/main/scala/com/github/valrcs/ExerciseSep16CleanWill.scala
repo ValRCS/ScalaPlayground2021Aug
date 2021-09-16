031d876b8a22bbfa2d08ec34c987f38cf3758d82
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-09-16 19:56:26 +0300

    Sep 16 exercise on cleaning

---
+++ src/main/scala/com/github/valrcs/ExerciseSep16CleanWill.scala
@@ -0,0 +1,10 @@
+package com.github.valrcs
+
+object ExerciseSep16CleanWill extends App {
+  val src = "src/resources/The Case of the Missing Will.txt"
+  val dst = "src/resources/The Case of the Missing Will_cleaned.txt"
+
+  //TODO read src
+  //TODO write to dst the cleaned version without the Project Gutenberg footer
+
+}
