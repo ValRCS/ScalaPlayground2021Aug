67c366b44d4f0cf0f28190fef1ba60ce3849bca5
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-09-27 21:27:13 +0300

    Sep 27 exercise solved, and serializer

--- src/main/scala/com/github/valrcs/ExerciseSep27XMLReading.scala
+++ src/main/scala/com/github/valrcs/ExerciseSep27XMLReading.scala
@@ -2,7 +2,7 @@ package com.github.valrcs
 
 import scala.xml.XML
 
-object ExerciseSep27XMLReading {
+object ExerciseSep27XMLReading extends App {
   val srcFile = "src/resources/xml/books.xml"
 
   val xml = XML.loadFile(srcFile) //so val xml is points to the root element of this particular document
@@ -11,5 +11,19 @@ object ExerciseSep27XMLReading {
   //Read the books from the xml put them inside the case file, keep all the atributes and tags
 
   //TODO print the book objects(created from case class) in Ascending order of prices
+  val bookNodes = xml \ "book" // we get some sequence of nodes
+
+  def nodeToBook(node: scala.xml.Node):Book = {
+    val category: String = node.attribute("category").getOrElse("No category").toString
+    val title: String = (node \ "title").text
+    val author: String  = (node \ "author").text
+    val year:String = (node \ "year").text
+    val price: Double = (node \ "price").text.toDouble
+    Book(category, title, author, year, price)
+  }
+
+  val books = bookNodes.map(nodeToBook) //so map is our loop
+  //of course we could do it with for (book <- bookNodes) yield nodeToBook(book)
+  books.sortBy(_.price).foreach(println)
 
 }
