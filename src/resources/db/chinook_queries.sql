6ee16d18800327ffcd0bf9977cbcf45d2377ea8f
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-10-05 20:10:03 +0300

    Oct 5th Exercises Do 12 and 13!

--- src/resources/db/chinook_queries.sql
+++ src/resources/db/chinook_queries.sql
@@ -42,6 +42,55 @@
 --JOIN employees e 
 --ON c.SupportRepId = e.EmployeeId ;
 
+--7. invoice_totals.sql: 
+--Provide a query that shows the 
+--Invoice Total, Customer name, Country and Sale Agent name for all invoices and customers.
+--SELECT i.Total, c.FirstName , c.LastName , e.FirstName , e.LastName FROM invoices i 
+--JOIN customers c 
+--ON c.CustomerId = i.CustomerId  -- careful with joins
+--JOIN employees e 
+--ON c.SupportRepId = e.EmployeeId ;
+
+--8 total_invoices_{year}.sql: How many Invoices were there in 2009 and 2011?
+--
+--Answer: "83" "83"
+
+--9. total_sales_{year}.sql: What are the respective total sales for each of those years?
+
+--Answer: "449.46" "469.58"
+--SELECT * FROM invoices i ;
+--SELECT date('now'), time('now'); --3 hours back so Greenwitch(sic?) time
+--https://stackoverflow.com/questions/13020611/how-to-select-rows-by-date-in-sqlite
+--SELECT COUNT(*), SUM(i2.Total) FROM invoices i2 
+--where i2.InvoiceDate >= datetime('2009-01-01 00:00:00')
+--  AND i2.InvoiceDate <  datetime('2010-01-01 00:00:00'); -- in a typed database
+----WHERE i2.InvoiceDate LIKE "2009%";
+
+--  SELECT COUNT(*), SUM(i2.Total) FROM invoices i2 
+--where i2.InvoiceDate >= datetime('2011-01-01 00:00:00')
+--  AND i2.InvoiceDate <  datetime('2012-01-01 00:00:00'); -- in a typed database
+
+--total_sales_{year}.sql: What are the respective total sales for each of those years?
+--
+--Answer: "449.46" "469.58"
+
+--10 invoice_37_line_item_count.sql: Looking at the InvoiceLine table, 
+-- provide a query that COUNTs the number of line items for Invoice ID 37.
+--SELECT COUNT(*) FROM invoice_items ii 
+--WHERE InvoiceId = 37;
+
+--11 This involves GROUP BY which we are going to cover after the break
+--line_items_per_invoice.sql: Looking at the InvoiceLine table, provide a query 
+--that COUNTs the number of line items for each Invoice. 
+--HINT: GROUP BY
+
+--12 - line_item_track.sql: Provide a query that includes the purchased track name with each invoice line item
+
+--13 line_item_track_artist.sql: 
+-- Provide a query that includes the purchased track name AND artist name with each invoice line item.
+
+--l: Provide a query that includes the purchased track name with each invoice line item.
+
 --SELECT
 --    Name, 
 --    Title
