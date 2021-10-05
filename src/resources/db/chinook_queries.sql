2a002cda02a5e54877721c36285ca6385d5df339
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-10-05 19:23:18 +0300

    3rd exercise

--- src/resources/db/chinook_queries.sql
+++ src/resources/db/chinook_queries.sql
@@ -1,12 +1,28 @@
-SELECT
-    Name, 
-    Title
-FROM
-    artists
-    -- so we get all left rows but right row might not have a match
-JOIN albums ON
-    artists.ArtistId = albums.ArtistId
-ORDER BY Name DESC;
+--1. Provide a query showing Customers 
+--(just their full names, customer ID and country) who are not in the US.
+--SELECT c.FirstName , c.LastName , c.CustomerId , c.Country FROM customers c 
+--WHERE c.Country != 'USA'
+--ORDER BY c.Country ASC;
+
+--2. brazil_customers.sql: Provide a query only showing the Customers from Brazil.
+--SELECT * FROM customers c 
+--WHERE c.Country = "Brazil";
+
+--3. brazil_customers_invoices.sql: 
+--Provide a query showing the Invoices of customers who are from Brazil. 
+--The resultant table should show the customer's full name, 
+--Invoice ID, Date of the invoice and billing country.
+
+
+--SELECT
+--    Name, 
+--    Title
+--FROM
+--    artists
+--    -- so we get all left rows but right row might not have a match
+--JOIN albums ON
+--    artists.ArtistId = albums.ArtistId
+--ORDER BY Name DESC;
 
 --SELECT a3.AlbumId , a3.Title , a4.Name FROM albums a3 
 ----INNER JOIN artists a4 
